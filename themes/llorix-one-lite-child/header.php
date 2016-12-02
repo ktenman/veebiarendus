d1868462144bbf11eb2899a9e5452fa51bd9e6f8
Kardo Jõeleht <kardoj@users.noreply.github.com>
2016-12-02 11:26:52 +0200

    Adds Google Analytics

--- themes/llorix-one-lite-child/header.php
+++ themes/llorix-one-lite-child/header.php
@@ -51,6 +51,19 @@ endif; ?>
 <!-- =========================
 	SECTION: HOME / HEADER
 ============================== -->
+<!-- Google Analytics -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-88370719-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<!-- End Google Analytics -->
+
 <!--header-->
 <header itemscope itemtype="http://schema.org/WPHeader" id="masthead" role="banner" data-stellar-background-ratio="0.5" class="header header-style-one site-header">
 
